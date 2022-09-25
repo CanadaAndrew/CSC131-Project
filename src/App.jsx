8c37465030f48955ed300022455b89aaac144508
ttang0 <65149380+ttang0@users.noreply.github.com>
2022-09-25 01:18:40 -0700

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,39 @@
 import React from "react";
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
-import { createTheme } from '@mui/material/styles';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 
 
+const theme1 = createTheme({
+    typography: 
+    {
+        fontFamily: [  '"Segoe UI Symbol"',  ].join(','),
+        fontSize: 24,
+    },
+    palette: 
+    {
+        primary: {main: "#ff8f00" }, // this changes the textbox to orange?
+
+        background: 
+        {
+            default: "#666666" //changes whole page background to gray
+        },
+      },
+  });
+
 const App = () => {
     return(
+        <ThemeProvider theme={theme1}> {/* ThemeProvider uses the theme function above*/}
         <div>
-           
-            <Typography align= "center" >    {/*Typography enables text editing*/}
+         <CssBaseline /> {/* This enables changing background color*/}
+            <Typography align= "center" color="common.black" >    {/*Typography enables text editing*/}
             <h1 id="h1"  > SS# Background Check</h1>
             <TextField id="filled-basic" label="Social Security #" variant="filled" />
-
             </Typography>
+           
         </div>
-        
+        </ThemeProvider>
   
     )
 }
--- src/index.js
+++ src/index.js
@@ -4,3 +4,6 @@ import ReactDom from 'react-dom';
 import App from './App';
 
 ReactDom.render(<App />, document.getElementById('root'));
+
+//document.body.style.backgroundColor = "#666666"; 
+//brute force change background color, no longer needed, changed with material ui 
